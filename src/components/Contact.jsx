b5dc9ea1350aaf490bdb4b2e35e9d3bdd25a8d13
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-06-30 08:27:42 +0700

    create new tab when click contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,13 +14,13 @@ const Contact = () => {
         </div>
         <div className = 'px-48 py-16 grid gap-16 xl:grid-cols-3 xl:grid-rows-2 md:grid-cols-2 md:grid-rows-3 sm:grid-cols-2 sm:grid-rows-3 border-solid '>
           <li className='flex flex-col px-4 py-4 justify-center rounded-md border-2 border-solid hover:bg-gray-700'>
-            <a href="https://open.spotify.com/playlist/3IgKEztujeLCWbLrq9C89d?si=253525e1b0304c99">My Sportify Playlist when I code ( Music is so louded )</a>
+            <a href="https://open.spotify.com/playlist/3IgKEztujeLCWbLrq9C89d?si=253525e1b0304c99" target='_blank'>My Sportify Playlist when I code ( Music is so louded )</a>
           </li>
           <li className='flex flex-col px-4 py-4 justify-center rounded-md border-2 border-solid hover:bg-gray-700'>
-            <a href='https://www.youtube.com/watch?v=nz7_2tyKOfk'>Mystery Box? Who know something in it?</a>
+            <a href='https://www.youtube.com/watch?v=nz7_2tyKOfk' target='_blank'>Mystery Box? Who know something in it?</a>
           </li>
           <li className='flex flex-col px-4 py-4 justify-center rounded-md border-2 border-solid hover:bg-gray-700'>
-            <a href='https://www.youtube.com/@Tifo'>My football's channel. Keep calm and share it!</a>
+            <a href='https://www.youtube.com/@Tifo' target='_blank'>My football's channel. Keep calm and share it!</a>
           </li>
         </div>
 
